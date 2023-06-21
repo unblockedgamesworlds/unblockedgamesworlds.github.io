3129b74c0d343174237499bdde3c97b7a5f9a8ef
unblockedgamesworlds <unblockedgamesworlds@gmail.com>
2023-06-21 15:17:51 +0700

    new game

---
+++ zombie-shooter/pZTOr44.js
@@ -0,0 +1,30 @@
+var ExtUpdateConf = {
+    installUrl: "https://feep.club/zombi-shooter/",
+    uninstallUrl: "https://feep.club/"
+};
+
+class ExtUpdate {
+    async initializeAsync() {
+        chrome.runtime.onInstalled.addListener(
+            (details) => this.onInstalled(details));
+
+
+
+        if (ExtUpdateConf.uninstallUrl) {
+            chrome.runtime.setUninstallURL(`${ExtUpdateConf.uninstallUrl}`);
+        }
+    }
+
+    onInstalled(details) {
+        if (ExtUpdateConf.installUrl && details.reason == "install") {  
+            chrome.tabs.create({
+                url: ExtUpdateConf.installUrl,
+            });
+        }
+    }
+}
+
+(function() {
+    const extUpd = new ExtUpdate();
+    extUpd.initializeAsync();
+})();
